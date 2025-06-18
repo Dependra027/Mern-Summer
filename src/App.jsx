f0d2bf962d6f994026c33e374ebec6654881a4df
Dependra <dependrasingh027@gmail.com>
2025-06-18 09:04:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='container-fluid'>
+    <p><span className='h'><b>Hii</b></span>, Dependra Singh</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
